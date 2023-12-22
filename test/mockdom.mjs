594a99104e9c588c5fe982fd16e2bd210c44f5c2
Roberto Ferranti <roberto.ferranti@gmail.com>
2023-12-22 15:15:31 +0100

    :ref: simplified mockdom

--- test/mockdom.mjs
+++ test/mockdom.mjs
@@ -3,25 +3,9 @@
 import { JSDOM } from "jsdom";
 
 
-const blacklisted = [
-    'localStorage',
-    'sessionStorage',
-    'setTimeout',
-    'setInterval',
-    'clearTimeout',
-    'clearInterval',
-    'queueMicrotask',
-    'console'
-];
-
 function mockdom(html) {
     let jsdom = new JSDOM(html);
-    for (let k in jsdom.window) {
-        if (blacklisted.indexOf(k) !== -1) {
-            continue;
-        }
-        globalThis[k] = jsdom.window[k];
-    }
+    globalThis.document = jsdom.window.document;
     globalThis.Node = jsdom.window.Node;
     globalThis.NodeList = jsdom.window.NodeList;
     globalThis.Element = jsdom.window.Element;
