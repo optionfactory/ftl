f37a5c62770b41923815eece3697599bc96f4078
Roberto Ferranti <roberto.ferranti@gmail.com>
2024-08-07 14:53:56 +0200

    :bug: Fragments.* is now using a tamplate tag to avoid html to be processed (e.g: a tr tag inside a div tag); :new: version 0.22.0

--- src/fragments.mjs
+++ src/fragments.mjs
@@ -1,13 +1,16 @@
 /* global DocumentFragment, HTMLTemplateElement */
 
 class Fragments {
+    /**
+     * 
+     * @param  {string} html 
+     * @returns 
+     */
     static fromHtml(html) {
-        const el = document.createElement('div');
+        const el = document.createElement("template");
         el.innerHTML = html;
-        const fragment = new DocumentFragment();
-        fragment.append(...el.childNodes);
-        return fragment;
-    }
+        return el.content;
+    }    
 }
 
 
