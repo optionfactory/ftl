320af4325c76f2c9407a6dfbc1f09c5c4a071add
Roberto Ferranti <roberto.ferranti@gmail.com>
2025-05-17 09:18:41 +0200

    :new: version 2.0.0: changed ParsedElement interface

--- src/elements.mjs
+++ src/elements.mjs
@@ -1,6 +1,24 @@
-import { Expressions } from "./expressions.mjs"
 import { Template } from "./template.mjs"
 import { Nodes, LightSlots } from "./dom.mjs";
+
+
+class UpgradeQueue {
+    #q = [];
+    constructor() {
+        document.addEventListener('DOMContentLoaded', () => this.#dequeue());
+    }
+    enqueue(el) {
+        if (!this.#q.length) {
+            requestAnimationFrame(() => this.#dequeue());
+        }
+        this.#q.push(el);
+    }
+    #dequeue() {
+        for (const el of this.#q.splice(0)) {
+            el.upgrade()
+        }
+    }
+}
 
 class ElementsRegistry {
     #tagToClass = {};
@@ -18,6 +36,7 @@ class ElementsRegistry {
     #components = {};
     #modules;
     #data = [];
+    #upgrades = new UpgradeQueue();
     defineTemplate(id, html) {
         if (html === null || html === undefined) {
             return undefined;
@@ -65,6 +84,30 @@ class ElementsRegistry {
     }
     configure() {
         for (const [tag, klass] of Object.entries(this.#tagToClass)) {
+            const { observed, template, templates, slots, mappers } = klass;
+        
+            const attrsAndTypes = (observed ?? []).map(a => {
+                const [attr, maybeType] = a.split(":");
+                const type = maybeType?.trim() ?? 'string';
+                if (!(type in this.#mappers)) {
+                    throw new Error(`unsupported attribute type: ${type}`);
+                }
+                return [attr.trim(), type];
+            });
+        
+            const attrsAndMappers = attrsAndTypes.map(([attr, type]) => [attr, mappers?.[type] ?? this.#mappers[type]]);
+            const attrToMapper = Object.fromEntries(attrsAndMappers);
+        
+            const templateNamesAndIds = Object.entries(Object.assign({}, templates, { default: template }) ?? {}).map(([k, v]) => [k, registry.defineTemplate(null, v)]);
+            const templateNameToId = Object.fromEntries(templateNamesAndIds);
+
+            klass.BITS = {
+                enqueue: (el) => this.#upgrades.enqueue(el),
+                SLOTS: slots,
+                ATTR_TO_MAPPER: attrToMapper,
+                ATTRS_AND_MAPPERS: attrsAndMappers,
+                TEMPLATE_NAME_TO_ID: templateNameToId
+            }
             customElements.define(tag, klass);
             delete this.#tagToClass[tag];
         }
@@ -86,121 +129,86 @@ class ElementsRegistry {
     component(name) {
         return this.#components[name];
     }
-    get mappers() {
-        return this.#mappers;
-    }
 }
 
 const registry = new ElementsRegistry();
 
+class ParsedElement extends HTMLElement {
+    static BITS = {
+        enqueue: (el) => {},
+        SLOTS: false,
+        ATTR_TO_MAPPER: {},
+        ATTR_AND_MAPPERS: [],
+        TEMPLATE_NAME_TO_ID: {}
+    }
+    static get observedAttributes() {
+        return Object.keys(this.BITS.ATTR_TO_MAPPER);
+    }
+    #parsed = false;
+    #reflecting = false;    
+    #bits(){
+        return /** @type {typeof ParsedElement} */(this.constructor).BITS;
+    }
+    template(name) {
+        return registry.template(this.#bits().TEMPLATE_NAME_TO_ID[name ?? 'default']);
+    }
+    connectedCallback() {
+        if (this.#parsed) {
+            return;
+        }
+        if (this.ownerDocument.readyState === 'complete' || Nodes.isParsed(this)) {
+            this.#bits().enqueue(this);
+            return;
+        }
+        const ac = new AbortController();
+        const clearAndQueue = () => { ac.abort(); observer.disconnect(); this.#bits().enqueue(this); }
+        this.ownerDocument.addEventListener('DOMContentLoaded', clearAndQueue, { signal: ac.signal });
+        const observer = new MutationObserver(() => {
+            if (!Nodes.isParsed(this)) {
+                return;
+            }
+            clearAndQueue();
+        });
+        const parent = /** @type {Node} */ (this.parentNode);
+        observer.observe(parent, { childList: true });
+    }
+    attributeChangedCallback(attr, oldValue, newValue) {
+        if (!this.#parsed || oldValue === newValue) {
+            return;
+        }
+        if (this.#reflecting) {
+            return;
+        }
+        const mapper = this.#bits().ATTR_TO_MAPPER[attr];
+        this[attr] = mapper(newValue, attr, this);
+    }
+    reflect(fn) {
+        this.#reflecting = true;
+        try {
+            fn();
+        } finally {
+            this.#reflecting = false;
+        }
+    }
+    render(c){
+    }
+    async upgrade() {
+        if (this.#parsed) {
+            return;
+        }
+        this.#parsed = true;
+        await this.render({
+            slots: this.#bits().SLOTS ? LightSlots.from(this) : undefined,
+            observed: Object.fromEntries(this.#bits().ATTRS_AND_MAPPERS.map(([attribute, mapper]) => [attribute, mapper(this.getAttribute(attribute), attribute, this)])),
+        });
 
-class UpgradeQueue {
-    #q = [];
-    constructor() {
-        document.addEventListener('DOMContentLoaded', () => this.#dequeue());
-    }
-    enqueue(el) {
-        if (!this.#q.length) {
-            requestAnimationFrame(() => this.#dequeue());
+        for (const [attr, mapper] of this.#bits().ATTRS_AND_MAPPERS) {
+            if (this.hasAttribute(attr)) {
+                this[attr] = mapper(this.getAttribute(attr), attr, this);
+            }
         }
-        this.#q.push(el);
-    }
-    #dequeue() {
-        for (const el of this.#q.splice(0)) {
-            el.upgrade()
-        }
-    }
+    }    
 }
 
-const ParsedElement = (conf) => {
-    const upgrades = globalThis.upgrades ??= new UpgradeQueue();
-
-    const { observed, template, templates, slots } = conf ?? {};
-
-    const attrsAndTypes = (observed ?? []).map(a => {
-        const [attr, maybeType] = a.split(":");
-        const type = maybeType?.trim() ?? 'string';
-        if (!(type in registry.mappers)) {
-            throw new Error(`unsupported attribute type: ${type}`);
-        }
-        return [attr.trim(), type];
-    });
-
-    const attrsAndMappers = attrsAndTypes.map(([attr, type]) => [attr, registry.mappers[type]]);
-    const attrToMapper = Object.fromEntries(attrsAndMappers);
-
-    const templateNamesAndIds = Object.entries(Object.assign({}, templates, { default: template }) ?? {}).map(([k, v]) => [k, registry.defineTemplate(null, v)]);
-    const templateNameToId = Object.fromEntries(templateNamesAndIds);
-
-    const k = class extends HTMLElement {
-        static get observedAttributes() {
-            return Object.keys(attrToMapper);
-        }
-        #parsed = false;
-        #reflecting = false;
-        constructor() {
-            super();
-        }
-        template(name) {
-            return registry.template(templateNameToId[name ?? 'default']);
-        }
-        connectedCallback() {
-            if (this.#parsed) {
-                return;
-            }
-            if (this.ownerDocument.readyState === 'complete' || Nodes.isParsed(this)) {
-                upgrades.enqueue(this);
-                return;
-            }
-            const ac = new AbortController();
-            const clearAndQueue = () => { ac.abort(); observer.disconnect(); upgrades.enqueue(this); }
-            this.ownerDocument.addEventListener('DOMContentLoaded', clearAndQueue, { signal: ac.signal });
-            const observer = new MutationObserver(() => {
-                if (!Nodes.isParsed(this)) {
-                    return;
-                }
-                clearAndQueue();
-            });
-            const parent = /** @type {Node} */ (this.parentNode);
-            observer.observe(parent, { childList: true });
-        }
-        attributeChangedCallback(attr, oldValue, newValue) {
-            if (!this.#parsed || oldValue === newValue) {
-                return;
-            }
-            if (this.#reflecting) {
-                return;
-            }
-            const mapper = attrToMapper[attr];
-            this[attr] = mapper(newValue, attr, this);
-        }
-        reflect(fn) {
-            this.#reflecting = true;
-            try {
-                fn();
-            } finally {
-                this.#reflecting = false;
-            }
-        }
-        async upgrade() {
-            if (this.#parsed) {
-                return;
-            }
-            this.#parsed = true;
-            // @ts-ignore
-            await this.render?.({
-                slots: slots ? LightSlots.from(this) : undefined,
-                observed: Object.fromEntries(attrsAndMappers.map(([attribute, mapper]) => [attribute, mapper(this.getAttribute(attribute), attribute, this)])),
-            });
-
-            for (const [attr, mapper] of attrsAndMappers) {
-                if (this.hasAttribute(attr)) {
-                    this[attr] = mapper(this.getAttribute(attr), attr, this);
-                }
-            }
-        }
-    };
-    return k;
-}
 
 export { ElementsRegistry, registry, ParsedElement };
